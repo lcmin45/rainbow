d26a7ee784ea17b802cbd942b350ff07e261183c
kimpudu <40376673+kimpudu@users.noreply.github.com>
2018-06-19 17:10:38 +0900

    추추추추우우우우우려겨려려려려어어어억

--- console/console/MAIN.cpp
+++ console/console/MAIN.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
-
+#include "daehyuk.h"
 
 using namespace std;
 
 int main()
 {
+	daehyuk _d;
+
+	_d.coutDaehyuk();
 
 	return 0;
 }
---
+++ console/console/daehyuk.cpp
@@ -0,0 +1,17 @@
+#include "daehyuk.h"
+
+
+
+daehyuk::daehyuk()
+{
+}
+
+
+daehyuk::~daehyuk()
+{
+}
+
+void daehyuk::coutDaehyuk(void)
+{
+	std::cout << "´ëÇõ" << '\n';
+}
